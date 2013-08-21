e367f9e2176e3f95dce34db9dac6bceea93a36c4
Yizhong Liang <yzleung.pku@gmail.com>
2013-08-21 15:33:18 +0200

    Valid Palindrome

---
+++ 125.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            while (l <= r && !Character.isLetterOrDigit(s.charAt(l))) l++;
+            while (l <= r && !Character.isLetterOrDigit(s.charAt(r))) r--;
+            if (l <= r && s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
