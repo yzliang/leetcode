e35b1e4e7dd39324f9a082244fa25cf979d5c45d
Yizhong Liang <yzleung.pku@gmail.com>
2013-09-04 00:14:46 +0200

    Unique Paths

---
+++ 62.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int> > num(m, vector<int>(n));
+        for (int i = 0; i < m; i++)
+            num[i][0] = 1;
+        for (int i = 0; i < n; i++)
+            num[0][i] = 1;
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                num[i][j] = num[i - 1][j] + num[i][j - 1];
+        return num[m - 1][n - 1];
+    }
+};
