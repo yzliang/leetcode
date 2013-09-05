5675f7da40476e6c43d9b4080719434cb996b40b
Yizhong Liang <yzleung.pku@gmail.com>
2013-09-05 11:06:52 +0200

    Valid Number

--- 65.cc
+++ 65.cc
@@ -16,32 +16,39 @@ public:
     bool check_base(const char* s) {
         // Trim left and remove leading sign.
         while (isspace(*s)) s++;
-        if (*s == '-') s++;
+        if (*s == '-' || *s == '+') s++;
         // Scan rest.
         bool has_dot = false;
         bool has_digit = false;
         while (*s && *s != 'e' && *s != 'E' && !isspace(*s)) {
-            if (*s == '-') {
-                return false;
-            } else if (*s == '.') {
+            if (*s == '.') {
                 if (has_dot) return false;
                 has_dot = true;
-            } else if (isdigit(s)) {
+            } else if (isdigit(*s)) {
                 has_digit = true;
             } else {
                 return false;
             }
             s++;
         }
-        while (isspace(*s)) s++;
+        bool has_space = false;
+        while (isspace(*s)) {
+          s++;
+          has_space = true;
+        }
         if (*s && *s != 'e' && *s != 'E') return false;
+        if (has_space && (*s == 'e' || *s == 'E')) return false;
         return has_digit;
     }
     
     bool check_exp(const char* s) {
-        if (*s == '-') s++;
-        while (isdigit(*s)) s++;
+        if (*s == '-' || *s == '+') s++;
+        bool has_digit = false;
+        while (isdigit(*s)) {
+            s++;
+            has_digit = true;
+        }
         while (isspace(*s)) s++;
-        return *s == '\0';
+        return *s == '\0' && has_digit;
     }
 };
