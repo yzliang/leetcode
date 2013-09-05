3147d11c7c57b53c2e2163e3df21c05d4a0f5177
Yizhong Liang <yzleung.pku@gmail.com>
2013-09-05 11:08:03 +0200

    Sqrt(x)

--- 69.cc
+++ 69.cc
@@ -4,7 +4,7 @@ public:
         double val = 1;
         while (true) {
             double new_val = (val + x / val) / 2;
-            if (statc_cast<int>(val) == static_vast<int>(new_val)) {
+            if (static_cast<int>(val) == static_cast<int>(new_val)) {
                 return static_cast<int>(val);
             }
             val = new_val;
