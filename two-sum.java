781d06331d92c0bed002f5df8744250a340fa3b4
Yizhong Liang <liang.yizhong@epfl.ch>
2013-10-05 09:51:09 +0200

    New version for two-sum.java

--- two-sum.java
+++ two-sum.java
@@ -2,21 +2,14 @@ import java.util.HashMap;
 
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        HashMap<Integer, Integer> index = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> numIndex = new HashMap<Integer, Integer>();
         for (int i = 0; i < numbers.length; i++) {
-            index.put(numbers[i], i);
-        }
-        for (int i = 0; i < numbers.length; i++) {
-            int other = target - numbers[i];
-            if (index.containsKey(other)) {
-                int[] answer = {i + 1, index.get(other) + 1};
-                if (answer[1] < answer[0]) {
-                    int temp = answer[0];
-                    answer[0] = answer[1];
-                    answer[1] = temp;
-                }
-                return answer;
+            Integer need_index = numIndex.get(target - numbers[i]);
+            if (need_index != null) {
+                int[] ans = {need_index + 1, i + 1};
+                return ans;
             }
+            numIndex.put(numbers[i], i);
         }
         return null;
     }
