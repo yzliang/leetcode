17b95c02754632db824170e701b58184afb81c06
Yizhong Liang <liang.yizhong@epfl.ch>
2013-10-08 15:46:36 +0200

    wildcard matching

--- wildcard-matching.java
+++ wildcard-matching.java
@@ -8,14 +8,18 @@ public class Solution {
             start++;
         if (start == p.length()) return true;
         int end = start + 1;
-        while (end < p.length() && p.charAt(end) == '*')
+        while (end < p.length() && p.charAt(end) != '*')
             end++;
+        if (end - start > s.length()) return false;
         String segment = p.substring(start, end);
         if (start == 0) {
             if (!matchCharacters(s.substring(start, end), segment))
                 return false;
             else
                 return isMatch(s.substring(end), p.substring(end));
+        } else if (end == p.length()) {
+            return matchCharacters(s.substring(s.length() - segment.length()),
+                        segment);
         } else {
             int len = end - start;
             for (int i = 0; i + len <= s.length(); i++) {
