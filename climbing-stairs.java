3e48887c39f1d7969556382eb41901d836368f46
Yizhong Liang <yzleung.pku@gmail.com>
2013-10-13 19:36:08 +0200

    climbing stairs

---
+++ climbing-stairs.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+        int a = 1, b = 2;
+        for (int i = 3; i <= n; i++) {
+            int temp = a;
+            a = b;
+            b += temp;
+        }
+        return b;
+    }
+}
