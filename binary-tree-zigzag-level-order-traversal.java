73e35f2c33315118b2500e4b47840b0aac581510
Yizhong Liang <yzleung.pku@gmail.com>
2013-10-20 11:06:57 +0200

    zigzag level order traversal

---
+++ binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
+        ArrayList<ArrayList<TreeNode>> queue =
+            new ArrayList<ArrayList<TreeNode>>();
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
+        if (root == null) return ans;
+        queue.add(new ArrayList<TreeNode>());
+        queue.get(0).add(root);
+        while (true) {
+            ArrayList<TreeNode> lastLevel = queue.get(queue.size() - 1);
+            ArrayList<TreeNode> newLevel = new ArrayList<TreeNode>();
+            for (TreeNode node : lastLevel) {
+                if (node.left != null) newLevel.add(node.left);
+                if (node.right != null) newLevel.add(node.right);
+            }
+            if (newLevel.isEmpty()) break;
+            queue.add(newLevel);
+        }
+        boolean reverse = false;
+        for (ArrayList<TreeNode> nodes : queue) {
+            ArrayList<Integer> values = new ArrayList<Integer>();
+            for (TreeNode node :nodes) values.add(node.val);
+            if (reverse) Collections.reverse(values);
+            ans.add(values);
+            reverse = !reverse;
+        }
+        return ans;
+    }
+}
